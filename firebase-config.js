4773e216ba5503116c374961b2d53c7233340f38
steo410 <steojhukna@gmail.com>
2026-09-22 16:25:05 +0900

    Connect Crestid Firebase web app configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,7 @@
 // Firebase 콘솔 > 프로젝트 설정 > 웹 앱의 공개 설정값. 서비스 계정 비밀키를 넣지 마세요.
-export default { apiKey: '', authDomain: '', projectId: '', appId: '' };
+export default {
+  apiKey: 'AIzaSyBNbFGl5504yfzWpl0r_52Nr3O9aKsSp70',
+  authDomain: 'crestid-f67ef.firebaseapp.com',
+  projectId: 'crestid-f67ef',
+  appId: '1:383981559829:web:ebe12a1463990424e4ee1c'
+};
